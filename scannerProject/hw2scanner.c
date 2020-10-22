f422efb80269b530fda3f3517d5e4f33f480d705
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-10-21 23:02:56 -0400

    Update hw2scanner.c

--- scannerProject/hw2scanner.c
+++ scannerProject/hw2scanner.c
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
     
     char c;
     char* word = (char*)calloc(IDENTIFIER_MAX_LENGTH + 1, sizeof(char));
-    int letter = 0, i;
+    int letter = 0, i, size = 0;
     while((c = fgetc(ipf)) != EOF) {
         //printf("%c", c);
         if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '=' || c == ',' || c == '.' || c == '<' || c == '>' || c == ';' || c == ':') {
