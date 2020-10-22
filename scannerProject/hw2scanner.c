063d85b3d8d69036159456a0e7e9a0589c1240fe
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-10-21 23:46:53 -0400

    Update hw2scanner.c

--- scannerProject/hw2scanner.c
+++ scannerProject/hw2scanner.c
@@ -29,8 +29,8 @@ int main(int argc, char* argv[]) {
     }
 
     // Reserved words
-    char* reservedWords[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"};
-    char specialSymbols[] = {'+','-','*','/','(',')','=',',','.','<','>',';',':'};
+    //char* word[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"};
+    //char specialSymbols[] = {'+','-','*','/','(',')','=',',','.','<','>',';',':'};
 
     char c;
     char* word = (char*)calloc(IDENTIFIER_MAX_LENGTH, sizeof(char));
@@ -70,18 +70,91 @@ int main(int argc, char* argv[]) {
             else if (strcmp(word, "odd") == 0)
                 temp.token = 8;
             
-            if (word[0] != '\0') {
-                printf("%s\n", word);
-                for (i = 0; i < IDENTIFIER_MAX_LENGTH; i++)
-                    word[i] = '\0';
-                letter = 0;
-            }
+            table[size-1] = temp;
+            table = (lexeme*)realloc(table, size+1);
+            size++;
+            
+            for (i = 0; i < IDENTIFIER_MAX_LENGTH; i++)
+                temp.identifier[i] = '\0';
+            
+            temp.identifier[0] = c;
+
+            if (c == '+')
+                temp.token = 28;
+            else if (c == '-')
+                temp.token = 29;
+            else if (c == '*')
+                temp.token = 30;
+            else if (c == '/')
+                temp.token = 27;
+            else if (c == '(')
+                temp.token = 21;
+            else if (c == ')')
+                temp.token = 22;
+            else if (c == '=')
+                temp.token = 23;
+            else if (c == ',')
+                temp.token = 24;
+            else if (c == '.')
+                temp.token = 33;
+            else if (c == '<')
+                temp.token = 25;
+            else if (c == '>')
+                temp.token = 26;
+            else if (c == ';')
+                temp.token = 32;
+            else if (c == ':')
+                temp.token = 31;
+
+            table[size-1] = temp;
+            table = (lexeme*)realloc(table, size+1);
+            size++;
+
+            printf("%s\n", word);
+            for (i = 0; i < IDENTIFIER_MAX_LENGTH; i++)
+                word[i] = '\0';
+            letter = 0;
         }
         else if (c != ' ' && c != '\t' && c != '\n') {
             word[letter] = c;
             letter++;
         }
         else {
+            lexeme temp;
+            strcpy(temp.identifier, word);
+            if (strcmp(word, "const") == 0)
+                temp.token = 28;
+            else if (strcmp(word, "var") == 0)
+                temp.token = 29;
+            else if (strcmp(word, "procedure") == 0)
+                temp.token = 30;
+            else if (strcmp(word, "call") == 0)
+                temp.token = 27;
+            else if (strcmp(word, "begin") == 0)
+                temp.token = 21;
+            else if (strcmp(word, "end") == 0)
+                temp.token = 22;
+            else if (strcmp(word, "if") == 0)
+                temp.token = 23;
+            else if (strcmp(word, "then") == 0)
+                temp.token = 24;
+            else if (strcmp(word, "else") == 0)
+                temp.token = 33;
+            else if (strcmp(word, "while") == 0)
+                temp.token = 25;
+            else if (strcmp(word, "do") == 0)
+                temp.token = 26;
+            else if (strcmp(word, "read") == 0)
+                temp.token = 32;
+            else if (strcmp(word, "write") == 0)
+                temp.token = 31;
+            else if (strcmp(word, "odd") == 0)
+                temp.token = 8;
+            
+            table[size-1] = temp;
+            table = (lexeme*)realloc(table, size+1);
+            size++;
+            
             if (word[0] != '\0') {
                 printf("%s\n", word);
                 for (i = 0; i < IDENTIFIER_MAX_LENGTH; i++)
@@ -89,8 +162,6 @@ int main(int argc, char* argv[]) {
                 letter = 0;
             }
         }
-
-        
     }
 
     fclose(ipf);
