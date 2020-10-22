6f1e12c402624665b956772952d6c3dd0645c18c
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-10-21 23:27:35 -0400

    Update hw2scanner.c

--- scannerProject/hw2scanner.c
+++ scannerProject/hw2scanner.c
@@ -1,7 +1,7 @@
 /* HW2 Scanner
 Team Dalton Kajander and Shawn Guydeene
 Due-date: 10/22/2020, 11:59
-Goal: scan
+Goal:
 */
 
 #include <stdio.h>
@@ -13,13 +13,12 @@ Goal: scan
 
 typedef struct {
     int token;
-    char identifier[];
+    char* identifier;
 } lexeme;
 
 int main(int argc, char* argv[]) {
     // argv[0] - name of this file itself
     // argv[1] - name of file to compile
-
 
     char* filename = argv[1];
     FILE* ipf = fopen(filename, "r");
@@ -29,17 +28,48 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Reserved words and special symbols
-    char* reservedWords[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"}; 
-    char specialSymbols[] = {'+','-','*','/','(',')','=',',','.','<','>',';',':'};
-    
+    // Reserved words
+    //char* word[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"};
+    //char specialSymbols[] = {'+','-','*','/','(',')','=',',','.','<','>',';',':'};
+
     char c;
-    char* word = (char*)calloc(IDENTIFIER_MAX_LENGTH + 1, sizeof(char));
-    lexeme* table = (lexeme*)malloc(sizeof(lexeme));
+    char* word = (char*)calloc(IDENTIFIER_MAX_LENGTH, sizeof(char));
+    lexeme* table = (lexeme*)malloc(sizeof(lexeme) * 1);
     int letter = 0, i, size = 0;
     while((c = fgetc(ipf)) != EOF) {
         //printf("%c", c);
         if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '=' || c == ',' || c == '.' || c == '<' || c == '>' || c == ';' || c == ':') {
+            lexeme temp;
+            strcpy(temp.identifier, word);
+            if (strcmp(word, "const") == 0)
+                temp.token = 28;
+            else if (strcmp(word, "var") == 0)
+                temp.token = 29;
+            else if (strcmp(word, "procedure") == 0)
+                temp.token = 30;
+            else if (strcmp(word, "call") == 0)
+                temp.token = 27;
+            else if (strcmp(word, "begin") == 0)
+                temp.token = 21;
+            else if (strcmp(word, "end") == 0)
+                temp.token = 22;
+            else if (strcmp(word, "if") == 0)
+                temp.token = 23;
+            else if (strcmp(word, "then") == 0)
+                temp.token = 24;
+            else if (strcmp(word, "else") == 0)
+                temp.token = 33;
+            else if (strcmp(word, "while") == 0)
+                temp.token = 25;
+            else if (strcmp(word, "do") == 0)
+                temp.token = 26;
+            else if (strcmp(word, "read") == 0)
+                temp.token = 32;
+            else if (strcmp(word, "write") == 0)
+                temp.token = 31;
+            else if (strcmp(word, "odd") == 0)
+                temp.token = 8;
+            
             if (word[0] != '\0') {
                 printf("%s\n", word);
                 for (i = 0; i < IDENTIFIER_MAX_LENGTH; i++)
