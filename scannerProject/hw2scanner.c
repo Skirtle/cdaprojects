430860b696d59f9c15c93b42b61523d828808338
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-10-21 22:08:10 -0400

    Update hw2scanner.c

--- scannerProject/hw2scanner.c
+++ scannerProject/hw2scanner.c
@@ -7,6 +7,9 @@ Goal:
 #include <stdio.h>
 #include <stdlib.h>
 
+#define IDENTIFIER_MAX_LENGTH 11
+#define NUMBER_MAX_LENGTH 5
+
 int main(int argc, char* argv[]) {
     // argv[0] - name of this file itself
     // argv[1] - name of file to compile
@@ -19,6 +22,9 @@ int main(int argc, char* argv[]) {
         printf("Filename %s invalid, please try again.", filename);
         return 2;
     }
+
+    // Reserved words
+    char* word[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"}; 
 
     return 1;
 }
