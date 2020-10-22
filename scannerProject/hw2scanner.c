8b11fc34dc13ff45920f2f2caa09ccc1a7534a37
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-10-21 22:23:45 -0400

    Update hw2scanner.c

--- scannerProject/hw2scanner.c
+++ scannerProject/hw2scanner.c
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Reserved words
-    char* word[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"}; 
+    char* reservedWords[] = {"const", "var", "procedure", "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "odd"}; 
     
     char c;
     char word[] = (char)calloc(IDENTIFIER_MAX_LENGTH, sizeof(char));
