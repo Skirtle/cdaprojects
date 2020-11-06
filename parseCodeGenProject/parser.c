d03248718bf3bec34b03ee21472a7d45d72419ea
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-11-06 17:16:20 -0500

    Update parser.c

--- parseCodeGenProject/parser.c
+++ parseCodeGenProject/parser.c
@@ -3,8 +3,11 @@
 #include "parser.h"
 #include "scanner.h"
 
-symbol* parse(lexeme *list, int *no_errors) {
-    symbol *table;
+symbol* parse(lexeme *list, int *no_errors, int list_size, int *table_size) {
+    symbol *table = (symbol*) calloc(1, sizeof(symbol) + 12);
+    int index = 0;
+
+
 
     return table;
 }
