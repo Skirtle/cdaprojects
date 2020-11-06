a5e075149a589767acc6c20fc04203d15f1a8562
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-11-06 08:29:15 -0500

    Update driver.c

--- parseCodeGenProject/driver.c
+++ parseCodeGenProject/driver.c
@@ -14,25 +14,53 @@ COP3402-20Fall 0001
 
 
 int main(int argc, char **argv) {
-    if (argc < 2)
-        printf("error : please include the file name");
+    if (argc < 2) {
+        printf("Error: No file to compile");
+        return 1;
+    }
+    // Flag declarations for all the command line arguments
+    int aFlag = 0, vFlag = 0, lFlag = 0;
+    if (argc == 5) {
+        aFlag = 1;
+        vFlag = 1;
+        lFlag = 1;
+    }
+    else if (argc != 2) {
+        if (argv[2][1] == 'v')
+            vFlag = 1;
+        else if (argv[2][1] == 'a')
+            aFlag = 1;
+        else
+            lFlag = 1;
+
+        if (argc = 4) {
+            if (argv[3][1] = 'v')
+                vFlag = 1;
+            else if (argv[2][1] == 'a')
+                aFlag = 1;
+            else
+                lFlag = 1;
+        }
+    }
     
     FILE *ipf = fopen(argv[1], "r");
 
+    // Grabbing the input file and storing it inside a char array.
     char *inputfile = malloc(500 * sizeof(char));
     char c = fgetc(ipf);
     int i = 0;
-    
     while(1) {
         inputfile[i++] = c;
         c = fgetc(ipf);
         if (c == EOF)
             break;
     }
-
     inputfile[i] == '\0';
 
-    printf("%s\n", inputfile);
+    // Printing the source program.
+    printf("\nSource Program:\n%s\n", inputfile);
+    // Set up the lexeme list
+    lexeme *list = scan(inputfile, lFlag);
 
     return 0;
 }
