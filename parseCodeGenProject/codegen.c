9fc720b791288c7423e50f4acc7ac2cf1a6893a6
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-11-06 11:32:33 -0500

    Add files via upload

---
+++ parseCodeGenProject/codegen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "scanner.h"
+#include "parser.h"
+#include "codegen.h"
+
+instruction* generate_code(symbol *table, lexeme *list) {
+    instruction *code;
+
+    return code;
+}
---
+++ parseCodeGenProject/parser.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
+#include "scanner.h"
+
+symbol* parse(lexeme *list, int *no_errors) {
+    symbol *table;
+
+    return table;
+}
