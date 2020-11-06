6993bdb3af5f903b94b6101982a409d726291d50
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-11-06 11:31:11 -0500

    Update codegen.h

--- parseCodeGenProject/codegen.h
+++ parseCodeGenProject/codegen.h
@@ -1,4 +1,16 @@
 #ifndef __CODEGEN_H
 #define __CODEGEN_H
 
+#include "scanner.h"
+#include "parser.h"
+
+typedef struct
+{
+    int opcode;
+    char *op;
+    int l;
+    int m;
+} instruction;
+
+
 #endif
