14e58a558de73628907f09b2b73f7175a4a9253e
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-11-06 11:21:47 -0500

    Update parser.h

--- parseCodeGenProject/parser.h
+++ parseCodeGenProject/parser.h
@@ -1,4 +1,17 @@
 #ifndef __PARSER_H
 #define __PARSER_H
 
+#include "scanner.h"
+
+typedef struct {
+    int kind;
+    char* name;
+    int val;
+    int level;
+    int addr;
+    int mark;
+} symbol;
+
+symbol* parse(lexeme *list, int *no_errors);
+
 #endif
